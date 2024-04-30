cb1238468e2de411856965765b17116ec8c21d89
amor1523 <amor152394@gmail.com>
2024-04-30 17:28:33 +0900

    [Feature] 플레이어 이름, 직업 선택 기능 구현
    그 외 기타 코드 수정

--- [A05]SpartaDungeonTextRpg/Player.cs
+++ [A05]SpartaDungeonTextRpg/Player.cs
@@ -2,28 +2,33 @@
 
 namespace _A05_SpartaDungeonTextRpg
 {
+    public enum Job
+    {
+        Knight,
+        Mage,
+        Archer
+    }
+
     public class Player
     {   
         Random rand  = new Random();
-        public string Name { get; }
-        public string Job { get; }
+        public string Name { get; set; }
+        public Job Job { get; set; }
         public int Level { get; }
         public int Atk { get; set; }
-        public int Def { get; }
+        public int Def { get; set; }
         public int HP { get; set; }
         public float Gold { get; set; }
         public bool IsDead => HP <= 0;
 
-        public Player(string name, string job, int level, int attack, int defense, int health, float gold)
+        public Player()
         {
-            Name = name;
-            Job = job;
-            Level = level;
-            Atk = attack;
-            Def = defense;
-            HP = health;
-            Gold = gold;
+            Level = 1;
+            Gold = 1500f;
+            
+      
         }
+
         public void TakeDamage(int damage)
         {
             HP -= damage;
--- [A05]SpartaDungeonTextRpg/Program.cs
+++ [A05]SpartaDungeonTextRpg/Program.cs
@@ -7,18 +7,26 @@ namespace SpartaDungeonTextRpg
 {
     public class GameManager
     {
+        Dictionary<Job, string> dict_Job = new Dictionary<Job, string>()
+            {
+                {Job.Knight, "전사" },
+                {Job.Mage, "마법사" },
+                {Job.Archer, "궁수" }
+            };
         private Player player;
         private List<Monster> monsters; // 몬스터 리스트 추가
         private Random random = new Random();
         public GameManager()
         {
             InitializeGame();
-
+            PlayerName();
+            PlayerClass();
+            MainMenu();
         }
 
         private void InitializeGame()
         {
-            player = new Player("Chad", "전사", 1, 10, 5, 100, 1500f);
+            player = new Player();
             monsters = new List<Monster>(); // 몬스터 리스트 초기화
 
             // 몬스터 생성 및 추가
@@ -27,6 +35,51 @@ namespace SpartaDungeonTextRpg
             monster.GenerateMonster(); // 몬스터 생성
             monsters.AddRange(monster.CreatedMonster); // 생성된 몬스터를 리스트에 추가
         }
+
+        private void PlayerName()
+        {
+            Console.Clear();
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.Write("원하시는 이름을 설정해주세요\n>> ");
+            player.Name = Console.ReadLine();
+        }
+
+        private void PlayerClass()
+        {
+            Console.Clear();
+            Console.WriteLine("이제 직업을 선택하실 수 있습니다.");
+            Console.WriteLine("각 직업마다 기본 스텟과 스킬이 다를 수 있습니다.");
+            Console.WriteLine("[1] 전사   | Atk: 10 Def: 5 Hp: 100");
+            Console.WriteLine("[2] 마법사 | Atk:  8 Def: 3 Hp: 80");
+            Console.WriteLine("[3] 궁수   | Atk: 13 Def: 4 Hp: 90");
+
+            int input = ConsoleUtility.PromptMenuChoice(1, 3);
+            switch (input)
+            {
+                case 1:
+                    player.Job = Job.Knight;
+                    player.Atk = 10;
+                    player.Def = 5;
+                    player.HP = 100;
+                    break;
+
+                case 2:
+                    player.Job = Job.Mage;
+                    player.Atk = 8;
+                    player.Def = 3;
+                    player.HP = 80;
+                    break;
+                
+                case 3:
+                    player.Job = Job.Archer;
+                    player.Atk = 13;
+                    player.Def = 4;
+                    player.HP = 90;
+                    break;
+            }
+    }
+
+
 
         public void MainMenu()
         {
@@ -56,7 +109,7 @@ namespace SpartaDungeonTextRpg
             ConsoleUtility.ShowTitle("[상태보기]");
             Console.WriteLine("캐릭터의 정보를 표시합니다.\n");
             Console.WriteLine($"Lv. {player.Level}");
-            Console.WriteLine($"{player.Name} ({player.Job})");
+            Console.WriteLine($"{player.Name} ({dict_Job[player.Job]})");
             Console.WriteLine($"공격력 :{player.Atk}");
             Console.WriteLine($"방어력 :{player.Def}");
             Console.WriteLine($"체력 :{player.HP}");
