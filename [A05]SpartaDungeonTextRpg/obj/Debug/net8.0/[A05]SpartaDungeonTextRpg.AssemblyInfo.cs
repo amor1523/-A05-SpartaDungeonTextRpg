2392991f3b5bfa1a47ab51b11ae10a2ba765153b
Usanim <167047045+KCngMn@users.noreply.github.com>
2024-05-01 10:14:20 +0900

    Add_MissAttack
    몬스터가 회피할 확률 추가

--- [A05]SpartaDungeonTextRpg/obj/Debug/net8.0/[A05]SpartaDungeonTextRpg.AssemblyInfo.cs
+++ [A05]SpartaDungeonTextRpg/obj/Debug/net8.0/[A05]SpartaDungeonTextRpg.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("[A05]SpartaDungeonTextRpg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea70ce0c18fb81c531ca0ecb7cc8486244fc1e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7329ef1a134bdee58da50fc93c9331978cfc5cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("[A05]SpartaDungeonTextRpg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("[A05]SpartaDungeonTextRpg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
