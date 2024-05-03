963ba75e894e3446d43cf688f2f21e86b09c9ae0
Usanim <167047045+KCngMn@users.noreply.github.com>
2024-05-03 19:07:13 +0900

    dd

--- [A05]SpartaDungeonTextRpg/obj/Debug/net8.0/[A05]SpartaDungeonTextRpg.AssemblyInfo.cs
+++ [A05]SpartaDungeonTextRpg/obj/Debug/net8.0/[A05]SpartaDungeonTextRpg.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("[A05]SpartaDungeonTextRpg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31502ce16100fec0019b07d5518256f82a029b79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207ead092e29b3e3b9b0936d16a779f93e0979ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("[A05]SpartaDungeonTextRpg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("[A05]SpartaDungeonTextRpg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
